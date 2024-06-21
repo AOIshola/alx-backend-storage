211c76e6a1a8f0ebea305257771a2ed1fe4c84f5
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-06-21 12:53:41 +0000

    use advanced sql concepts

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,10 @@
--- creates a table users with id, email, and name
-CREATE TABLE IF NOT EXISTS 'users' (
-	'id' int NOT NULL AUTO_INCREMENT PRIMARY_KEY,
-	'email' VARCHAR(255) NOT NULL UNIQUE,
-	'name' VARCHAR(255)
+-- Create the table users if it doesn't already exist
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
 
+-- Ensure the email column is unique
 ALTER TABLE users ADD UNIQUE (email);

3c8fdb6d2899484cdb9de3720a5eafb6e024aff0
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-06-21 13:00:01 +0000

    use advanced sql concepts

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,10 +1,4 @@
 -- creates a table users with attributes id, email, name, and country
-CREATE TABLE IF NOT EXISTS 'users' (
-        'id' int NOT NULL AUTO_INCREMENT PRIMARY_KEY,
-        'email' VARCHAR(255) NOT NULL UNIQUE,
-        'name' VARCHAR(255),
-)
-
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE IF NOT EXISTS users (
@@ -14,5 +8,3 @@ CREATE TABLE IF NOT EXISTS users (
     country ENUM('US', 'CO', 'TN') NOT NULL,
     PRIMARY KEY (id)
 );
-
-ALTER TABLE users ADD UNIQUE (email);
